4b4da6b0b63207e784aaf2ad34c7f18b9e199aa6
Maya <48359706+mayaesar@users.noreply.github.com>
2025-08-01 18:11:12 -0400

    Feat/about me (#2)
    * WIP on feat/hero-responsive
    
    * merge issue
    
    * fix

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,18 +3,18 @@
 <template>
   <section class="flex-1 flex flex-col items-center mt-6">
     <div class="flex flex-col items-center max-w-3xl w-full mx-auto">
-      <div class="mb-6">
+      <div>
         <img
           src="/portfolioText.png"
           alt="Maya Esar Avatar"
           class="w-96 md:w-full z-0"
         />
       </div>
-      <div class="mb-8">
+      <div>
         <img
           src="/SimplificationImg.png"
           alt="Maya Esar Portfolio"
-          class="w-64 md:w-96 z-10 -mt-40 md:-mt-72"
+          class="w-64 md:w-96 z-10 -mt-36 md:-mt-64"
         />
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
 
 <template>
   <div class="flex flex-col min-h-screen bg-gray-100">
-    <div class="flex-1 flex flex-col">
+    <div class="flex-1 flex flex-col mx-2.5">
       <NavBar />
       <Hero />
+      <AboutMe />
     </div>
   </div>
 </template>
