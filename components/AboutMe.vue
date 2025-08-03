0fc847beaa69bcde98eb6ed35ddff9d3e324619e
Maya <mayaesar@gmail.com>
2025-08-03 12:00:49 -0400

    about me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div class="flex-1 flex flex-col mt-6">
-    <h2 class="text-2xl font-bold">Hi, I'm Maya</h2>
+    <h2 class="text-4xl font-bold">Hey!</h2>
     <!-- potential paragraph -->
-    <p>
-      I'm a curious developer who loves turning “how the heck do I build this?”
-      into “heck yeah, I built this.” Currently documenting my journey from
-      junior to Jedi, one project, course, and caffeine-fueled late night at a
-      time. If you like learning, laughing, or debugging existential crises,
-      we'll probably get along.
+    <p class="text-lg mt-3">
+      I'm Maya, a curious developer who loves turning “how the heck do I build
+      this?” into “heck yeah, I built this.” Currently documenting my journey
+      from junior to Jedi, one project, course, and caffeine-fueled late night
+      at a time. If you like learning, laughing, or debugging existential
+      crises, we'll probably get along.
     </p>
   </div>
 </template>
